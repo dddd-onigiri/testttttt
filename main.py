f84da6a325ad2bb45a957950233aa13050e34104
Daiki Ito <39673489+dddd-onigiri@users.noreply.github.com>
2023-04-25 17:24:07 +0900

    Aiすげぇわ

--- main.py
+++ main.py
@@ -9,11 +9,23 @@ st.title("掲示板アプリ")
 # メッセージを入力するテキストボックスを表示する
 message = st.text_area("メッセージを入力してください")
 
+# メッセージを保存するリスト
+messages = []
+
 # 「投稿する」ボタンがクリックされた場合に実行される処理
 if st.button("投稿する"):
     # メッセージが入力されている場合は、掲示板に投稿する
     if message:
-        st.write("投稿されました:", message)
+        messages.append(message)
+        st.success("メッセージを投稿しました。")
     # メッセージが入力されていない場合は、エラーメッセージを表示する
     else:
         st.error("メッセージが入力されていません。")
+
+# メッセージを表示する
+if messages:
+    st.write("----- メッセージ一覧 -----")
+    for i, message in enumerate(messages):
+        st.write(f"{i+1}. {message}")
+else:
+    st.write("投稿されたメッセージはありません。")
