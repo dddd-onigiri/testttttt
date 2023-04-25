b189c0b89bca34f4322a1ab8039ebd4546fe970e
Daiki Ito <39673489+dddd-onigiri@users.noreply.github.com>
2023-04-25 17:22:36 +0900

    Aiのおかげ

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 import streamlit as st
 
-st.set_page_config(page_title="Pythonで遊ぶやつ", layout="wide")
+# ページのタイトルを設定する
+st.set_page_config(page_title="掲示板アプリ")
 
-st.title("BMI 計算機（Streamlitデモ）")
+# ページにタイトルを表示する
+st.title("掲示板アプリ")
 
-height_cm = st.number_input("身長（cm）", min_value=100, max_value=250)
+# メッセージを入力するテキストボックスを表示する
+message = st.text_area("メッセージを入力してください")
 
-weight_kg = st.number_input("体重（kg）", min_value=20, max_value=250)
-
-bmi = weight_kg / (height_cm / 100) ** 2
-
-st.write("あなたのBMIは", bmi, "です。")
-
-if bmi < 18.5:
-    st.write("やせ型です")
-elif bmi < 24.9:
-    st.write("標準です")
-elif bmi < 29.9:
-    st.write("肥満気味です")
-else:
-    st.write("肥満です")
-
-
-message = st.text_area("なんか入力")
-
-if st.button("なんか投稿", key=0):
-  st.write(message)
+# 「投稿する」ボタンがクリックされた場合に実行される処理
+if st.button("投稿する"):
+    # メッセージが入力されている場合は、掲示板に投稿する
+    if message:
+        st.write("投稿されました:", message)
+    # メッセージが入力されていない場合は、エラーメッセージを表示する
+    else:
+        st.error("メッセージが入力されていません。")
