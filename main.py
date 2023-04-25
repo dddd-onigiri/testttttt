f3b9f2f00cefa494bd553bbe3f93ff3ac6da4b5e
Daiki Ito <39673489+dddd-onigiri@users.noreply.github.com>
2023-04-25 17:26:27 +0900

    あああ

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 
 # ページのタイトルを設定する
 st.set_page_config(page_title="掲示板アプリ")
@@ -16,7 +17,11 @@ messages = []
 if st.button("投稿する"):
     # メッセージが入力されている場合は、掲示板に投稿する
     if message:
-        messages.append(message)
+        messages.append({
+            "id": len(messages) + 1,
+            "date": datetime.now(),
+            "message": message
+        })
         st.success("メッセージを投稿しました。")
     # メッセージが入力されていない場合は、エラーメッセージを表示する
     else:
@@ -25,7 +30,8 @@ if st.button("投稿する"):
 # メッセージを表示する
 if messages:
     st.write("----- メッセージ一覧 -----")
-    for i, message in enumerate(messages):
-        st.write(f"{i+1}. {message}")
+    sorted_messages = sorted(messages, key=lambda x: x["date"], reverse=True)
+    for i, message in enumerate(sorted_messages):
+        st.write(f"{i+1}. ID:{message['id']} / 日付:{message['date'].strftime('%Y-%m-%d %H:%M:%S')} / メッセージ:{message['message']}")
 else:
     st.write("投稿されたメッセージはありません。")
